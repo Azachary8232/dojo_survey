fc1c96f28c265cc5ec63d6153a07eee6f890955d
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-31 00:16:12 -0600

    Form Data Linked To Session

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, request
 app = Flask(__name__)
+app.secret_key = "skittles"
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/results')
+def result():
+    return render_template('results.html')
+
+@app.route('/process', methods = ["POST"])
+def form_process():
+    session['full_name'] = request.form['full_name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    print(session['location'])
+    return redirect('/results')
 
 
 if __name__ == "__main__":
